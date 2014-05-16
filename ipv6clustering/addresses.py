44120aa9f9f7cc0ffdd1902622570c1e216544e8
Peter Snyder <snyderp@gmail.com>
2014-05-16 15:03:20 -0500

    bughhg

---
+++ ipv6clustering/addresses.py
@@ -0,0 +1,6 @@
+"""Tools for manipulating and comparing IPv6 addresses.  These functions and
+classes are primarly for comparing \64 networks (to preprocess addresses
+for clustering to see what live allocations have been."""
+
+import ipaddress
+
